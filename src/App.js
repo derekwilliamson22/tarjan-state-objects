511bbe8bbb8cd9994f646b7e807a97728771bb0f
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-16 14:44:32 -0500

    added about me content with states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,9 @@ class App extends Component {
   state = {
     cohortName: 'Tarjan',
     salutation: 'Hello',
-    name: 'Derek'
-
+    name: 'Derek',
+    whereILive: 'Minnesota',
+    pizzaTopping: 'olives'
   }
 
   render() {
@@ -17,6 +17,13 @@ class App extends Component {
        <h1>{this.state.cohortName} Learns State with Objects</h1>
        <div>
          {this.state.salutation}! My name is {this.state.name}.
+       </div>
+       <div>
+       <ul>
+         <li>My name is {this.state.name}.</li>
+         <li>I live in {this.state.whereILive}</li>
+         <li>My favorite pizza topping is {this.state.pizzaTopping}</li>
+       </ul>
        </div>
       </div>
     );
