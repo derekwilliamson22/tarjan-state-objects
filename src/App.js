fc963339f948c40bdd10c937cd86c377fe444bd9
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-16 14:36:07 -0500

    added new states

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    cohortName: 'Tarjan',
+    salutation: 'Hello',
+    name: 'Derek'
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>{this.state.cohortName} Learns State with Objects</h1>
+       <div>
+         {this.state.salutation}! My name is {this.state.name}.
+       </div>
       </div>
     );
   }
