e30d6bc39e9f5bd8deae7ac737cabc9e93de66a8
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-16 16:46:24 -0500

    added form feature

--- src/Creatures/Creatures.js
+++ src/Creatures/Creatures.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Creatures extends Component {
 
     state = {
+        newCreatureName: '',
         creatures: [
         'Unicorn',
         'Sphinx',
@@ -10,9 +11,26 @@ class Creatures extends Component {
         'Dryad',
         'Jackalope'
         ]
-
     };
 
+    onChangeCreatureName = (event) => {
+        let newCreatureName = event.target.value;
+        this.setState({
+            newCreatureName: newCreatureName
+        });
+    }
+
+    onAddCreature = () => {
+        console.log('new creature is:', this.state.newCreatureName);
+
+        this.setState({
+            newCreatureName: '',
+            creatures: [
+            ...this.state.creatures, 
+            this.state.newCreatureName
+            ]
+        });
+    }
 
     render() {
      // let creatureListItems = [ ];
@@ -34,19 +52,23 @@ class Creatures extends Component {
 
         return (
             <div>
+                <input 
+                  type="text" 
+                  placeholder="New Creature Name"
+                  value={this.state.newCreatureName}
+                  onChange={this.onChangeCreatureName}
+                />
+                <button onClick={this.onAddCreature}>Add Creature</button>
                 <ul>
-                {this.state.creatures.map(creature =>
-                  <li>{creature}</li>
-                )}
+                  {this.state.creatures.map((creature, i) =>
+                    <li key={i}>
+                      {creature}
+                    </li>
+                  )}
                 </ul>
             </div>
         );
-
-
-
     }
-
-  
 } // end component
 
 export default Creatures;
